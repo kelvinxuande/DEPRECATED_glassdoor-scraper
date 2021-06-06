6c4d1bef98a3e4d5bc568459e891632176777234
kelvintan <kelvinxuande@gmail.com>
2021-06-06 13:43:26 +0800

    swapped tqdm for enlighten to improve console cleanliness

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 from time import time
 import csv
-from tqdm import tqdm
+import enlighten
 # custom
 from packages.common import requestAndParse
 from packages.page import extract_maximums, extract_listings
@@ -46,9 +46,6 @@ def fileWriter(listOfTuples, output_fileName):
 
 
 if __name__ == "__main__":
-    # initialise variables:
-    page_index = 1
-    total_listingCount = 0
 
     # load user defined configurations
     base_url, target_num = load_configs(path="data\config.json")
@@ -63,10 +60,17 @@ if __name__ == "__main__":
 
     # extract maximum number of jobs
     maxJobs, maxPages = extract_maximums(base_url)
-    print("[INFO] Maximum number of jobs in range: {}, number of pages in range: {}".format(maxJobs, maxPages))
+    # print("[INFO] Maximum number of jobs in range: {}, number of pages in range: {}".format(maxJobs, maxPages))
     if (target_num >= maxJobs):
         print("[ERROR] Target number larger than maximum number of jobs. Exiting program...\n")
         os._exit(0)
+
+    # initialise enlighten manager
+    manager = enlighten.get_manager()
+
+    # initialise variables
+    page_index = 1
+    total_listingCount = 0
 
     # initialise prev_url as base_url
     prev_url = base_url
@@ -75,21 +79,24 @@ if __name__ == "__main__":
         # clean up buffer
         list_returnedTuple = []
 
-        # update url
         new_url = update_url(prev_url, page_index)
-
-        print("\n[INFO] Processing page index {}: {}".format(page_index, new_url))
-
         page_soup,_ = requestAndParse(new_url)
         listings_set, jobCount = extract_listings(page_soup)
+        enlighten_progress = manager.counter(total=len(listings_set), desc="Listings processed", unit="LP", color="blue", leave=False)
+
+        print("\n[INFO] Processing page index {}: {}".format(page_index, new_url))
         print("[INFO] Found {} links in page index {}".format(jobCount, page_index))
 
-        for listing_url in tqdm(listings_set):
+        for listing_url in listings_set:
+
             # to implement cache here
 
             returned_tuple = extract_listing(listing_url)
             list_returnedTuple.append(returned_tuple)
             # print(returned_tuple)
+            enlighten_progress.update()
+
+        enlighten_progress.close()
 
         fileWriter(listOfTuples=list_returnedTuple, output_fileName=output_fileName)
 
