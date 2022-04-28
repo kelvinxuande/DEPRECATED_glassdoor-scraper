8076bed5ee04e3244278850910c1c2bd4e97bc21
Gang Li <lgang@umich.edu>
2022-04-28 09:42:24 +0800

    Page error fix

--- src/packages/page.py
+++ src/packages/page.py
@@ -12,14 +12,15 @@ except ModuleNotFoundError:
 # extract maximum number of jobs stated, only applicable for the "base" url
 def extract_maximums(base_url):
     page_soup,_ = requestAndParse(base_url)
-
+    print(page_soup)
     tmp_match_1 = [item for item in page_soup.find_all("p") if "data-test" in item.attrs][0]
-    tmp_match_2 = [item for item in page_soup.find_all("div") if "data-test" in item.attrs][-1]
+    tmp_match_2 = [item for item in page_soup.find_all("div") if "data-test" in item.attrs][-2]
+    
     maxJobs_raw = tmp_match_1.get_text()    # e.g. 7,764 Jobs
     maxPages_raw = tmp_match_2.get_text()   # e.g. Page 1 of 30
 
     try:
-        assert "Jobs" in maxJobs_raw
+        assert "jobs" in maxJobs_raw
         assert "Page" in maxPages_raw
     except Exception as e:
         print(e)
@@ -27,6 +28,8 @@ def extract_maximums(base_url):
 
     maxJobs = re.sub(r"\D", "", maxJobs_raw)
     maxPages = re.sub(r"\D", "", maxPages_raw)[1:]
+    
+    
     
     return(int(maxJobs), int(maxPages))
 
